b369ef0e830173808b10de11c51258ba68f08fa3
Naseef03 <muhammednaseef03@gmail.com>
2022-06-14 15:35:45 +0530

    Add playRound function
    playRound function decides winner of the round by looking at selections
    of both Player(taken through prompt) and Computer(taken through calling
    computerPlay function).

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ function computerPlay() {
       return 'scissors'
   }
 }
+
+// Decide the winner of the round 
+function playRound(playerSelection, computerSelection) {
+  switch(playerSelection.toLowerCase()) {
+    // Player choose rock
+    case 'rock':
+      switch(computerSelection) {
+        case 'rock':
+          return 'Tie! Rock and Rock';
+        case 'paper':
+          return 'You Lose! Paper beats Rock';
+        case 'scissors':
+          return 'You Win! Rock beats Scissors';
+      }
+    
+    // Player choose paper
+    case 'paper':
+      switch(computerSelection) {
+        case 'rock':
+          return 'You Win! Paper beats Rock';
+        case 'paper':
+          return 'Tie! Paper and Paper';
+        case 'scissors':
+          return 'You Lose! Scissors beats Paper';
+      }
+    
+    // Player choose scissors
+    case 'scissors':
+      switch(computerSelection) {
+        case 'rock':
+          return 'You Lose! Rock beats scissors';
+        case 'paper':
+          return 'You Win! Scissors beats Paper';
+        case 'scissors':
+          return 'Tie! Scissors and Scissors';
+      }
+  }
+}
